ce59ddcde9688474d932af84fc039b84776de5d9
John Mark Schofield <jms@schof.org>
2013-05-16 14:58:04 -0700

    Vbox guest additions not installed because of kernel upgrade. Fixed.

--- init.pp
+++ init.pp
@@ -89,6 +89,12 @@ class first {
     exec{"/usr/bin/wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | /bin/sh":
     }
 
+    # Fix vbox guest additions not installed because of kernel upgrade
+    exec {"/etc/init.d/vboxadd setup":
+        user => root,
+        group => root,
+    }
+
 }
 
 
