a33d2cf3f8aef6bcfa99837992916e2e9be1f0d3
John Mark Schofield <jms@schof.org>
2013-05-16 15:40:57 -0700

    More debug code.

--- resetdb_local.bash
+++ resetdb_local.bash
@@ -4,9 +4,11 @@ cd /vagrant/settings
 
 for SETTINGSFILE in settings_all settings_auth_local settings_local
 do
+    echo "Loading settings from $SETTINGSFILE"
     while read line
     do
         eval export $line
+        echo eval export $line
     done < $SETTINGSFILE
 done
 
