f55e540834128a58495ee09a9c7cddb5fa5f9ce6
John Mark Schofield <jms@schof.org>
2013-05-29 13:50:33 -0700

    Removing clean option from pg-restore, which seems to make it work without errors.

--- resetdb_local.bash
+++ resetdb_local.bash
@@ -2,31 +2,18 @@
 
 set -e
 
-# cd /vagrant/hfu_settings
-
-# for SETTINGSFILE in settings_all settings_auth_local settings_local
-# do
-#     echo "Loading settings from $SETTINGSFILE"
-#     while read line
-#     do
-#         eval export $line
-#     done < $SETTINGSFILE
-# done
-
 cd /vagrant
-
-source /home/vagrant/www/bin/activate
 
 set +e
 psql -U postgres -l | grep -q www
 if [ $? -eq 0 ]; then
     # DB exists
     set -e
-    psql -U postgres -c "drop database www;"
+    dropdb -U postgres www
 fi
 
 set -e
 
-psql -U postgres -c "create database www;"
+createdb -U postgres www
 
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d www /vagrant/prod.dump
+pg_restore --verbose --no-acl --no-owner -h localhost -U postgres -d www /vagrant/prod.dump
