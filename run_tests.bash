7dabfdb5acf818d9e1a1d11f7cbad0df22fb6241
John Mark Schofield <jms@schof.org>
2013-05-29 15:05:43 -0700

    Minor cleanups.

--- run_tests.bash
+++ run_tests.bash
@@ -2,9 +2,7 @@
 
 set -e
 
-cd /home/vagrant
-mkdir -p testing
+rm -rf /home/vagrant/testing
+mkdir -p /home/vagrant/testing
 cd /home/vagrant/testing
 wget -e robots=off --recursive -N --page-requisites --span-hosts -H -Ds'amazonaws.com,dakim-www-test.herokuapp.com' --no-remove-listing  --level=inf --no-directories --save-headers --tries 1 http://dakim-www-test.herokuapp.com
-
-
