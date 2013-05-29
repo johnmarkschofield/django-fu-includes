2d9fd355cb850bb4f97a1e64c1e1938dbadfb736
John Mark Schofield <jms@schof.org>
2013-05-29 15:09:26 -0700

    Had hardcoded URLs. Oops.

--- run_tests.bash
+++ run_tests.bash
@@ -2,7 +2,19 @@
 
 set -e
 
+cd /vagrant/hfu_settings
+
+for SETTINGSFILE in settings_all settings_auth_test settings_test
+do
+    echo "Loading settings from $SETTINGSFILE"
+    while read line
+    do
+        eval export $line
+    done < $SETTINGSFILE
+done
+
+
 rm -rf /home/vagrant/testing
 mkdir -p /home/vagrant/testing
 cd /home/vagrant/testing
-wget -e robots=off --recursive -N --page-requisites --span-hosts -H -Ds'amazonaws.com,dakim-www-test.herokuapp.com' --no-remove-listing  --level=inf --no-directories --save-headers --tries 1 http://dakim-www-test.herokuapp.com
+wget -e robots=off --recursive -N --page-requisites --span-hosts -H -Ds'amazonaws.com,$HOST' --no-remove-listing  --level=inf --no-directories --save-headers --tries 1 http://$HOST
