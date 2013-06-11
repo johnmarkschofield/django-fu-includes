8273eb86b64688afc87313cb1b34c1ab29964145
John Mark Schofield <jms@schof.org>
2013-06-11 10:23:00 -0700

    Making test a little more resilient.

--- run_tests.bash
+++ run_tests.bash
@@ -17,4 +17,10 @@ done
 rm -rf /home/vagrant/testing
 mkdir -p /home/vagrant/testing
 cd /home/vagrant/testing
-wget -e robots=off --recursive -N --page-requisites --span-hosts -H -Ds'amazonaws.com,$HOST' --no-remove-listing  --level=inf --no-directories --save-headers --tries 1 http://$HOST
+
+# Warm up the server
+sleep 5
+curl -o /dev/null http://$HOST
+wget -e robots=off --recursive -N --page-requisites \
+    --span-hosts -H -Ds'amazonaws.com,$HOST' --no-remove-listing  \
+    --level=inf --no-directories --save-headers --tries 1 http://$HOST
