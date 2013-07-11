e8dd84865b070398d0343b8ef088ecd7bf90dd9a
John Mark Schofield <jms@schof.org>
2013-07-11 15:38:28 -0700

    Whoops. Collectstatic script was loading wrong configs.

--- collectstatic.bash
+++ collectstatic.bash
@@ -3,7 +3,7 @@
 cd /vagrant/hfu_settings
 
 if [ "$1" =  "prod" ]; then
-    for SETTINGSFILE in settings_all settings_auth_prod settings_local
+    for SETTINGSFILE in settings_all settings_auth_prod settings_prod
     do
         echo "Loading settings from $SETTINGSFILE"
         while read line
@@ -12,7 +12,7 @@ if [ "$1" =  "prod" ]; then
         done < $SETTINGSFILE
     done
 elif [ "$1" = "test" ]; then
-    for SETTINGSFILE in settings_all settings_auth_test settings_local
+    for SETTINGSFILE in settings_all settings_auth_test settings_test
     do
         echo "Loading settings from $SETTINGSFILE"
         while read line
@@ -21,7 +21,16 @@ elif [ "$1" = "test" ]; then
         done < $SETTINGSFILE
     done
 elif [ "$1" = "staging" ]; then
-    for SETTINGSFILE in settings_all settings_auth_staging settings_local
+    for SETTINGSFILE in settings_all settings_auth_staging settings_staging
+    do
+        echo "Loading settings from $SETTINGSFILE"
+        while read line
+        do
+            eval export $line
+        done < $SETTINGSFILE
+    done
+elif [ "$1" = "local" ]; then
+    for SETTINGSFILE in settings_all settings_auth_local settings_local
     do
         echo "Loading settings from $SETTINGSFILE"
         while read line
@@ -30,7 +39,7 @@ elif [ "$1" = "staging" ]; then
         done < $SETTINGSFILE
     done
 else
-    echo "Invalid command line. Must be $0 and prod, staging, or test."
+    echo "Invalid command line. Must be $0 and prod, staging, or test or local."
     exit 100
 fi
 
