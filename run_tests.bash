47c0987727609073c43d53955bacb88a5f9c307a
John Mark Schofield <jms@schof.org>
2013-07-13 18:49:34 -0700

    Now fixing quoting

--- run_tests.bash
+++ run_tests.bash
@@ -22,8 +22,8 @@ cd /home/vagrant/testing
 sleep 5
 curl -o /dev/null http://$HOST
 wget -e robots=off --recursive -N --page-requisites \
-    --span-hosts -H -Ds'amazonaws.com,$HOST' --no-remove-listing  \
+    --span-hosts -H -Ds"amazonaws.com,$HOST" --no-remove-listing  \
     --level=inf --no-directories --save-headers --tries 1 https://$HOST
 echo wget -e robots=off --recursive -N --page-requisites \
-    --span-hosts -H -Ds'amazonaws.com,$HOST' --no-remove-listing  \
+    --span-hosts -H -Ds"amazonaws.com,$HOST" --no-remove-listing  \
     --level=inf --no-directories --save-headers --tries 1 https://$HOST
